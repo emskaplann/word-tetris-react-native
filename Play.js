8a8465f9671a834ecd439b32f9afc24754ab7be2
Emirhan Kaplan <emskaplann@gmail.com>
2019-11-21 10:21:06 -0500

    fetchWords added. gameBox rendered properly.

--- Play.js
+++ Play.js
@@ -2,28 +2,52 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { styles } from './App.js';
 
+  const playScreenStyles = {
+    text: {
+      color: '#fff'
+    }, gameBox: {
+      position: 'absolute',
+      top: 15,
+      backgroundColor: '#fff',
+      height: '50%',
+      width: '100%',
+    }
+  }
+
+  const words = []
 
 export default class PlayScreen extends React.Component {
   constructor(){
     super();
     this.state = {
-      timer: 3
+      timer: 3,
+      gameBoxRendered: false
     }
   }
+
+
   componentDidMount(){
     this.interval = setInterval(this.decreaseTimer, 1000)
   }
 
   shouldComponentUpdate(nextProps, nextState){
-    if(this.state !== nextState){
+    if(this.state !== nextState || this.state.timer === 'finished'){
       return true
     }
     return false
   }
 
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+
   decreaseTimer = () => {
-    if(this.state.timer !== 1){
+    if(this.state.timer !== 1 && this.state.timer !== 'Go!'){
       this.setState({timer: this.state.timer - 1})
+    } else if(this.state.timer === 'Go!'){
+      clearInterval(this.interval)
+      this.setState({timer: 'finished'})
+      this.setState({gameBoxRendered: true})
     } else {
       this.setState({timer: 'Go!'})
     }
@@ -40,11 +64,52 @@ export default class PlayScreen extends React.Component {
     }
   }
 
+// fetching words from NEWS API...
+  fetchWords = () => {
+    let url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=277ca875809f4f6484e5d830b2158bef'
+    fetch(url)
+    .then(r => r.json())
+    .then(response => {
+      response.articles.forEach(article => {
+      if (article.description != null) {
+          article.description.split(" ").forEach(word => {
+              word = word.replace(/[^a-zA-Z0-9 -]/g,"")
+              if( word == "" || word == " " || word == "--" ){
+
+              } else {
+                words.push(word.toLowerCase())
+              }
+              // fetch works properly
+         })
+        }
+      })
+    })
+  }
+
+
+
+  renderGameBox = () => {
+    if(this.state.timer === 'finished'){
+      console.log('true')
+      return (
+                <View style={playScreenStyles.gameBox}>
+                </View>
+                )
+    } else {
+      // just for test purposes :)
+      console.log('true2')
+      return(<Text style={playScreenStyles.text}>Something went wrong :/</Text>)
+    }
+  }
+
   render(){
       return (
         <View style={styles.container}>
-          <Text style={styles.text}>Word Tetris!!!!</Text>
             { this.timer() }
+            { this.renderGameBox() }
+            { this.fetchWords() }
         </View>
       );
     }
