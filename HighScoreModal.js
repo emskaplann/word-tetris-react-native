989cc8ec276e9b94584d414339a3d57cd16b0fcd
Emirhan Kaplan <emskaplann@gmail.com>
2019-11-25 17:15:37 -0500

    savepoint modal

--- HighScoreModal.js
+++ HighScoreModal.js
@@ -16,16 +16,16 @@ export default class HSModal extends React.Component {
           animationType="slide"
           transparent="false"
           visible={this.state.visible}
-          onRequestClose={() => {}}
+          onRequestClose={() => {this.setState({visible: false})}}
           >
           <View style={{height: "36%", width: "90%", padding: 10, borderWidth: 1, borderRadius: 10, borderColor: "#fff", position: 'absolute', top: 300, backgroundColor: "#000000", alignSelf: "center"}}>
             <TouchableHighlight
                 onPress={() => {
                   this.setState({visible: false});
                 }}>
-                <Text style={{color: "#fff", fontWeight: 'bold', fontSize: 20, position: 'absolute', top: 0, right: 10}}>X</Text>
+                <Text style={{color: "#fff", fontWeight: 'bold', fontSize: 30, alignSelf: 'flex-end',}}>x</Text>
               </TouchableHighlight>
-              <Text style={{color: "#fff", fontWeight: 'bold', fontSize: 50}}>your stats</Text>
+              <Text style={{color: "#fff", fontWeight: 'bold', fontSize: 50, position: 'absolute', top: 10, paddingLeft: 5}}>your stats</Text>
           </View>
         </Modal>
       </View>
